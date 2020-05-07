c2a70f831e4b69d0318d9c3a0faa035cb806118a
adzinets <adzinetskatsiaryna@gmail.com>
2020-05-07 17:51:51 +0300

    Updates

---
+++ precache-manifest.d24daa984bb9e0f6a60759b9f8714fc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf63aea838f34b21f5db9405573a9a77",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "2f3b9e11459469730445",
+    "url": "/counter/static/css/main.1ebb0b4d.chunk.css"
+  },
+  {
+    "revision": "14c8ff6c77c864a712f5",
+    "url": "/counter/static/js/2.69745904.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.69745904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f3b9e11459469730445",
+    "url": "/counter/static/js/main.2a387138.chunk.js"
+  },
+  {
+    "revision": "ce8f69cf3ef7ca3fcc57",
+    "url": "/counter/static/js/runtime-main.011d2541.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/counter/static/media/logo.5d5d9eef.svg"
+  }
+]);
